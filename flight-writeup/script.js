b8646c77e4d6eba3c091b36fe67c8254a10080a7
Chris Samperisi <chrissamperisi@3DR-BITD033-MBA.local>
2015-09-04 09:14:58 -0700

    Add script for converting URLs to preview links, add placeholder

---
+++ flight-writeup/script.js
@@ -0,0 +1,58 @@
+(function () {
+	function init() {
+		$(function() {
+			var iframe_width = 250,
+				iframe_height = '';
+			function convertYouTubeUrl(url) {
+			
+				var regEx = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+				var match = url.match(regEx);
+				
+				if(match && match[2].length == 11) {
+					return "<iframe src='https://www.youtube.com/embed/"+match[2]+"' frameborder='0' allowfullscreen width='"+iframe_width+"' height='"+iframe_height+"'></iframe>";
+				} else {
+					return false;
+				}
+		
+			}
+			function convertGoogleUrl(url) {
+				var regEx = /^.*(drive.google\/|\/d\/|\?id=)([^\/#\&\?]*).*/;
+				var match = url.match(regEx);
+				if(match && match[2].length == 28) {
+					return "<iframe src='https://drive.google.com/a/3drobotics.com/file/d/"+match[2]+"/preview' frameborder='0' allowfullscreen  width='"+iframe_width+"' height='"+iframe_height+"'></iframe>";
+				} else {
+					return false;
+				}
+			}
+			
+			var placeholder = $('<div>').addClass('placeholder').html("<span class='glyphicon glyphicon-warning-sign'></span><br/ >Video Not Found");
+			$('.video_embed').each(function () {
+				console.log('hi');
+			
+				var iframe = false;
+				var href=$(this).data('url');
+				if(iframe = convertYouTubeUrl(href)) {
+					$(this).html(iframe);
+				} else if(iframe=convertGoogleUrl(href)) {
+					$(this).html(iframe);
+				} else {
+					$(this).html(placeholder);
+				};
+			});
+		});
+		
+		
+	}
+	// Load jquery
+	var script = document.createElement("script");
+	script.src = 'https://code.jquery.com/jquery-1.11.3.min.js';
+	script.type = 'text/javascript';
+	
+	document.body.appendChild(script);
+// safe loading
+	script.onload = function () {
+		init()
+	};
+	
+
+})();
