05ef9d5c8b147feb81f3fc5ba7a187b2b797f29c
Chris Samperisi <chrissamperisi@3DR-BITD033-MBA.local>
2015-09-11 14:48:44 -0700

    Clean up the script

--- flight-writeup/script.js
+++ flight-writeup/script.js
@@ -1,6 +1,9 @@
+var embed_videos=false;
+
 (function () {
 	function init() {
 		$(function() {
+		
 			var iframe_width = '220px',
 				iframe_height = '';
 			function convertYouTubeUrl(url) {
@@ -24,25 +27,44 @@
 					return false;
 				}
 			}
-			
-			
-			$('.video_embed').each(function () {
-				var iframe = false;
-				var href=$(this).data('url');
-				if(iframe = convertYouTubeUrl(href)) {
-			
-					$(this).html(iframe);
-				} else if(iframe=convertGoogleUrl(href)) {
-					$(this).html(iframe);
-				} else {
-				
-					if(href="") {
-						$(this).addClass("placeholder").html("No Video Provided");
-					} else {					
-						$(this).addClass("placeholder").html("Video Not Found");
-					}
-				};
+			// select the target node
+			var target = document.querySelector('#chart1');
+
+			// create an observer instance
+			var observer = new MutationObserver(function(mutations) {
+			  mutations.forEach(function(mutation) {
+				console.log(mutation.type);
+				embed_videos()
+			  });    
 			});
+
+			// configuration of the observer:
+			var config = { attributes: true, childList: true, characterData: true };
+
+			// pass in the target node, as well as the observer options
+			observer.observe(target, config);
+
+			
+			embed_videos = function() {
+				$('.video_embed').each(function () {
+					var iframe = false;
+					var href=$(this).data('url');
+					if(iframe = convertYouTubeUrl(href)) {
+		
+						$(this).html(iframe);
+					} else if(iframe=convertGoogleUrl(href)) {
+						$(this).html(iframe);
+					} else {
+			
+						if(href="") {
+							$(this).addClass("placeholder").html("No Video Provided");
+						} else {					
+							$(this).addClass("placeholder").html("Video Not Found");
+						}
+					};
+				});
+			};
+			embed_videos();
 		});
 		
 		
